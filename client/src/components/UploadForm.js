3de93ef3c1c02445dacc1cb4689370cc19e43cef
peppermint100 <krpeppermint100@gmail.com>
2020-10-07 12:13:37 +0900

    added text input, changed some styles in form

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -8,32 +8,47 @@ const ENDPOINT = env.ENDPOINT
 const UploadForm = () => {
     const containerStyle = {
         position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
+       
         marginTop: "100px",
         height: "20vh"
     }
 
-    const inputStyle = {
+    const formStyle = {
+         display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: "200px"
+    }
+
+    const fileInputStyle = {
         display: "none"
     }
 
-    const buttonStyle ={
+    const textInputStyle = {
+        all: "unset",
+        width: "500px",
+        height: "30px",
+        border: "1.5px solid #bdc3c7",
+    }
+
+    const labelStyle ={
         all:"unset",
         display: "block",
         cursor: "pointer",
         fontSize: "24px",
         color: "#555555",
-        transform: "translate(-25%, 50%)"
     }
 
-    const labelStyle ={
+    const buttonStyle ={
+        all: "unset",
+        display: "block",
+        width: "120px",
+        textAlign: "center",
         padding: "12px 18px",
         cursor: "pointer",
-        borderRadius: "5px",
         backgroundColor: "#2980b9",
+        borderRadius: "5px",
         fontSize: "16px",
         fontWeight: "bold",
         color: "#fff"
@@ -54,16 +69,21 @@ const UploadForm = () => {
         }
     }
 
-    const onChange = (e) => {
+    const onSelectFile = (e) => {
         setFile(e.target.files[0])
+    }
+
+    const onChangeText = (e) => {
+
     }
 
     return (
         <div style={containerStyle}>
-            <form onSubmit={onSubmit}>
-                <label htmlFor="upload" style={labelStyle}>Select File</label>
-                <input onChange={onChange} type="file" id="upload" style={inputStyle}/>
-                <button style={buttonStyle} type="submit">🎈 Upload This Image</button>
+            <form onSubmit={onSubmit} style={formStyle}>
+                <label htmlFor="upload" style={labelStyle}>🎈 Select File</label>
+                <input onChange={onSelectFile} type="file" id="upload" style={fileInputStyle}/>
+                <input onChange={onChangeText} type="text" style={textInputStyle} placeholder="How's it going?" />
+                <button style={buttonStyle} type="submit"> Upload </button>
             </form>
             <section style={{transform: "translate(-20%, 90%)"}}>
                 { file ? (
